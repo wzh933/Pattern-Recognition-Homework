70c8648bc7c95944a0e723b5b7819c8263e193c9
Disenchanted <74145501+wzh933@users.noreply.github.com>
2022-04-30 15:23:01 +0800

    Add files via upload

---
+++ Pattern Recognition/Read_mat_file/ReadMatFile.py
@@ -0,0 +1,22 @@
+import scipy.io as scio
+
+
+class ReadMatFile:
+    # 读取出向量
+    def read_mat_file(self):
+        result_data = []
+        mat_data = scio.loadmat(self.file_path)['DAT']
+        row = len(mat_data[0])
+        col = len(mat_data[0][0])
+        for r in range(row):
+            for c in range(col):
+                x = []
+                for data in mat_data:
+                    x.append(data[r][c])
+                x = np.matrix(x).transpose()
+                result_data.append(x)
+        return result_data
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.sample = self.read_mat_file()
