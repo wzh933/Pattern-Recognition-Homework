a28effdd89408ec7efbb304bda3fd32efc37bda0
Disenchanted <74145501+wzh933@users.noreply.github.com>
2022-05-15 21:35:48 +0800

    Add files via upload

---
+++ Pattern Recognition/PCA/MLP.py
@@ -0,0 +1,37 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from PCA import PCA
+
+# 公开数据集
+faces = datasets.fetch_olivetti_faces()
+
+X = faces.data  # 数据
+y = faces.target  # 标签
+
+# -----------得到pca降维后的结果------------#
+# pca_X = PCA(sample=X, lower_dimension=200).data
+# np.save(file='pca_X.npy', arr=pca_X)
+# ---------------------------------------#
+pca_X = np.load('pca_X.npy')  # 降维后的数据
+scaler = StandardScaler()  # 均值方差标准化
+# 五折交叉检验
+# 必须要洗牌打乱，不然在这个数据集里，训练集和测试集的label基本不相交
+kf = KFold(n_splits=5, shuffle=True)
+
+# 多层感知机（神经网络）对象，50隐层
+clf = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(100,), random_state=1, max_iter=500)
+for train_index, test_index in kf.split(y):  # 划分数据集
+    train_X, test_X = pca_X[train_index], pca_X[test_index]
+    train_y, test_y = y[train_index], y[test_index]
+    scaler.fit(train_X)  # 计算得到标准化的均值方差只在训练集做，根据sklearn官方文档：Don't cheat
+    train_X = scaler.transform(train_X)
+    test_X = scaler.transform(test_X)
+    clf.fit(train_X, train_y)
+    predict_y = clf.predict(test_X)
+    acc = accuracy_score(predict_y, test_y)
+    print('accuracy: {:.2%}'.format(acc))
+
--- Pattern Recognition/PCA/PCA.py
+++ Pattern Recognition/PCA/PCA.py
@@ -9,37 +9,51 @@ class PCA:
         xs = sample
 
         # step1：对样本去均值，进行中心化
-        X = xs[0]  # 将样本中所有向量横向拼接
+        print("step1：对样本去均值，进行中心化")
+        X = xs[0]  # 将样本中所有向量竖向拼接
         for i in range(1, len(xs)):
-            X = np.hstack((X, xs[i]))
-        avg = np.mean(X, axis=1)
-        X_adj = X - avg
+            X = np.vstack((X, xs[i]))
+        avg = np.mean(X, axis=0)
+        X_adj = X - avg  # 去均值
 
         # step2：求特征协方差矩阵
-        sigma = np.cov(X_adj)  # sigma是协方差矩阵
+        print("step2：求特征协方差矩阵")
+        sigma = np.cov(X_adj.transpose())  # sigma是协方差矩阵
 
         # step3：求协方差矩阵的 特征值 和 特征向量
+        print("step3：求协方差矩阵的 特征值 和 特征向量")
         eigen_value, feature_vectors = np.linalg.eig(sigma)
 
         # step4：将 特征值-特征向量 对按特征值从大到小排序，选择前lower_dimension个
+        print("step4：将 特征值-特征向量 对按特征值从大到小排序，选择前lower_dimension个")
         eigen_feature_list = []
         for i in range(len(eigen_value)):
             eigen_feature_list.append((eigen_value[i], feature_vectors[i]))
         eigen_feature_list = sorted(eigen_feature_list, key=lambda tuple: tuple[0], reverse=True)
 
-        # step5：合并前lower_dimension个特征向量得到降维变换矩阵(dimension_reduction_mat)lower_dimension*orig_dimension
+        # step5：合并前lower_dimension个特征向量得到降维变换矩阵[dimension_reduction_mat]lower_dimension*orig_dimension
+        print("step5：合并前lower_dimension个特征向量得到降维变换矩阵(dimension_reduction_mat)lower_dimension*orig_dimension")
         dimension_reduction_mat = eigen_feature_list[0][1]
         for i in range(1, lower_dimension):
             dimension_reduction_mat = np.vstack((dimension_reduction_mat, eigen_feature_list[i][1]))
 
         # step6：进行降维变换
+        print("step6：进行降维变换")
         lower_xs = []
         for x in xs:
-            lower_x = np.matmul(dimension_reduction_mat, x)
+            lower_x = np.matmul(x, dimension_reduction_mat.transpose())
             lower_xs.append(lower_x)
-        self.lower_sample = lower_xs
+        # self.lower_sample = lower_xs
+        lower_xs = np.array(lower_xs).astype(np.float)  # 避免出现复数
+        self.data = lower_xs
+        # self.data = []
+        # for lower_x in lower_xs:
+        #     x = []
+        #     for dim in lower_x:
+        #         x.append(dim)
+        #     self.data.append(np.array(x))
+        # self.data = np.array(self.data)
 
-
-s = ReadMatFile('AR_120_14_50_45.mat').sample
-pca = PCA(sample=s, lower_dimension=1000)
-print(pca.lower_sample[0].shape)
+# s = ReadMatFile('AR_120_14_50_45.mat').sample
+# pca = PCA(sample=s, lower_dimension=1000)
+# print(pca.lower_sample[0].shape)
